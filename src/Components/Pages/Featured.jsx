3215addca84e80ae5bfe9564c541267f158633f1
nahida30 <nahidazmn@gmail.com>
2023-11-11 18:17:22 +0600

    parallax commit

--- src/Components/Pages/Featured.jsx
+++ src/Components/Pages/Featured.jsx
@@ -4,9 +4,9 @@ import './Featured.css'
 
 const Featured = () => {
     return (
-        <div className="featured-item text-white pt-8 my-20">
+        <div className="featured-item bg-fixed text-white pt-8 my-20">
             <SectionTitle subHeading="check it out" heading="Featured Item"></SectionTitle>
-            <div  className="md:flex justify-center items-center pb-20 pt-12 px-36">
+            <div  className="md:flex justify-center items-center bg-state-500 bg-opacity-60 pb-20 pt-12 px-36">
                 <div>
                     <img src={featured} alt="" />
                 </div>
@@ -14,7 +14,7 @@ const Featured = () => {
                     <p>Aug 20, 2029</p>
                     <p className="uppercase">Where can i get some?</p>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae fugit iure dolores, amet ducimus labore cum reiciendis nihil assumenda delectus rem nobis ipsa dolor sequi a accusantium voluptates iste non.</p>
-                    <button className="btn btn-outline mt-2">Button</button>
+                    <button className="btn btn-outline mt-2 border-0 border-b-4">Button</button>
                 </div>
             </div>
         </div>
